318f9ce43ba1992ecf02babfe9fbd09af4792d32
Dima K <@users.noreply.github.com>
2024-07-10 16:40:17 +0200

    Weather forecast with some style improvement

--- weather.py
+++ weather.py
@@ -1,12 +1,11 @@
 # pylint: disable=missing-module-docstring
 
 import sys
-import urllib.parse
 import requests
 
-BASE_URI = "https://weather.lewagon.com/"
-Geo_Directive = "geo/1.0/direct?q="
-Weather_Forecast = "/data/2.5/forecast?"
+BASE = "https://weather.lewagon.com/"
+GEO = "geo/1.0/direct?q="
+FOR = "/data/2.5/forecast?"
 
 
 def search_city(query):
@@ -14,7 +13,7 @@ def search_city(query):
     Look for a given city. If multiple options are returned, have the user choose between them.
     Return one city (or None)
     '''
-    response = requests.get(f"{BASE_URI}{Geo_Directive}{query}&limit=5",params={'name': query, 'format': 'json', 'jscmd':'data'},).json()
+    response = requests.get(f"{BASE}{GEO}{query}&limit=5",params={'name': query,'format': 'json','jscmd':'data'},).json()
     for i, x in enumerate(range(len(response)),start=1):
         city = response[x]['name']+ ", " + response[x]['country']
         print(f"{i} - {city}")
@@ -31,7 +30,7 @@ def search_city(query):
             try:
                 choice = int(input("Select a city from the options (number): "))
                 if 1 <= choice <= len(response):
-                    print(f"You have chosen option {choice}! This is {response[choice-1]['name']}, {response[x]['country']}")
+                    print(f"You have chosen option {choice}!")
                     chosen_city = response[choice-1]
                     return chosen_city
                 else:
@@ -42,7 +41,7 @@ def search_city(query):
 
 def weather_forecast(lat, lon):
     '''Return a 5-day weather forecast for the city, given its latitude and longitude.'''
-    forecast = requests.get(f"{BASE_URI}{Weather_Forecast}lat={lat}&lon={lon}", params={'format': 'json', 'jscmd':'data'},).json()
+    forecast = requests.get(f"{BASE}{FOR}lat={lat}&lon={lon}", params={'format': 'json', 'jscmd':'data'},).json()
     return forecast['list']
 
 
@@ -54,9 +53,9 @@ def main():
     lat = chosen_city['lat']
     lon = chosen_city['lon']
     forecast = weather_forecast(lat,lon)
-    for x in range(0,40,8):
-        if "15:00:00" in forecast[x]['dt_txt']:
-            print(f"{forecast[x]['dt_txt'][0:10]}: {forecast[x]['weather'][0]['description'].capitalize()}, {round(forecast[x]['main']['temp_max']-273,1)}")
+    for x_x in range(0,40,8):
+        if "15:00:00" in forecast[x_x]['dt_txt']:
+            print(f"{forecast[x_x]['dt_txt'][0:10]}: {forecast[x_x]['weather'][0]['description'].capitalize()}, {round(forecast[x_x]['main']['temp_max']-273,1)}")
 
 
 if __name__ == '__main__':
